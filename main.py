e78fe8ff6833d52555491eef4257518c16e31f21
jousef1 <yousefel560@gmail.com>
2023-07-07 20:03:10 +0800

    2

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ def makeCommits(dates):
     os.system('git push')
 
 # Specify the desired commit dates in the format "YYYY-MM-DD"
-commit_dates = ["2023-01-03", "2023-01-01", "2023-01-2"]
+commit_dates = ["2023-01-03", "2023-01-01", "2023-01-02" , "2023-01-04", "2023-01-05", "2023-01-06", "2023-01-07", "2023-01-08", "2023-01-09", "2023-01-10", "2023-01-11", "2023-01-12"]
 
 makeCommits(commit_dates)
